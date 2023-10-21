fd5fa793ec207fc71f98c7dc07afa93652017eab
yuichkun <yuichkun@gmail.com>
2023-10-22 00:13:28 +0900

    install contentful

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,8 @@
 /// <reference types="astro/client" />
 
-declare module 'dom-to-image-more';
+declare module "dom-to-image-more";
+interface ImportMetaEnv {
+  readonly CONTENTFUL_SPACE_ID: string;
+  readonly CONTENTFUL_DELIVERY_TOKEN: string;
+  readonly CONTENTFUL_PREVIEW_TOKEN: string;
+}
