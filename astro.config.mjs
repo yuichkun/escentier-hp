f52f0e239f8378b3849e6755ac50e8bbf287e665
yuichkun <yuichkun@gmail.com>
2023-10-22 15:16:42 +0900

    install glslify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,15 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 
-import glsl from "vite-plugin-glsl";
 import svelte from "@astrojs/svelte";
+import glsl from "vite-plugin-glsl";
+import glslify from "vite-plugin-glslify";
 
 export default defineConfig({
   integrations: [tailwind(), svelte()],
   output: "server",
   adapter: vercel(),
   vite: {
-    plugins: [glsl()],
+    plugins: [glsl(), glslify()],
   },
 });
