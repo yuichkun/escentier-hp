66ef0402c43aecb943a1549cd1b737e778c8d954
yuichkun <yuichkun@gmail.com>
2023-10-23 00:13:22 +0900

    add gallery bg transition effect

--- src/components/Gallery.svelte
+++ src/components/Gallery.svelte
@@ -1,8 +1,20 @@
-<div class="section" />
+<script>
+  let alpha = 0.2;
+
+  const onScroll = () => {
+    alpha = Math.min(window.scrollY / window.innerHeight, 1);
+  };
+
+  $: cssVarStyles = `--alpha:${alpha}`;
+</script>
+
+<svelte:window on:scroll={onScroll} />
+
+<div class="section" style={cssVarStyles} />
 
 <style>
   .section {
     height: 200vh;
-    background-color: rgba(var(--purple3), 0.2);
+    background-color: rgba(var(--purple3), var(--alpha));
   }
 </style>
