141e91593939e56810aef6f4efbf31e7cfd4e6b1
yuichkun <yuichkun@gmail.com>
2023-10-23 00:53:57 +0900

    add color themes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        blue: "#5005F2",
+        black: "#182E59",
+        purple1: "#D2BBF2",
+        purple2: "#9D85F2",
+        purple3: "#7E5EF2",
+      },
+    },
+  },
+  plugins: [],
+};
