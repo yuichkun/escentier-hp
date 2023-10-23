a3fda0672edb9d5394424d88c59c8fd3ea99cf0e
yuichkun <yuichkun@gmail.com>
2023-10-23 22:59:46 +0900

    fix infinity error

--- src/scripts/useScroll.ts
+++ src/scripts/useScroll.ts
@@ -1,5 +1,6 @@
 export function getScrollPosition() {
   const total = document.body.offsetHeight - window.innerHeight;
+  if (total === 0) return 0;
   const { scrollY } = window;
   return scrollY / total;
 }
