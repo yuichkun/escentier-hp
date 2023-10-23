8549fc943f92bd7edd4e583451460bf742e2d10a
yuichkun <yuichkun@gmail.com>
2023-10-23 21:53:33 +0900

    add video gallery

--- src/components/Gallery.svelte
+++ src/components/Gallery.svelte
@@ -1,5 +1,17 @@
-<script>
+<script lang="ts">
   let alpha = 0.2;
+
+  type Work = {
+    title: string;
+    name: string;
+    file: {
+      url: string;
+    };
+  };
+
+  export let works: Work[];
+
+  const videos = works.map((w) => `https:${w.file.url}`);
 
   const onScroll = () => {
     alpha = Math.min(window.scrollY / window.innerHeight, 1);
@@ -11,13 +23,41 @@
 <svelte:window on:scroll={onScroll} />
 
 <div class="section" style={cssVarStyles}>
-  <canvas class="w-full h-full" />
+  <div class="column-wrapper">
+    <div class="column">
+      {#each videos as url}
+        <video src={url} autoplay playsinline muted loop />
+      {/each}
+    </div>
+    <div class="column">
+      {#each videos as url}
+        <video src={url} autoplay playsinline muted loop />
+      {/each}
+    </div>
+    <div class="column">
+      {#each videos as url}
+        <video src={url} autoplay playsinline muted loop />
+      {/each}
+    </div>
+  </div>
 </div>
 
 <style>
   .section {
     height: 100vh;
+    overflow: hidden;
     background-color: rgb(var(--purple3));
     opacity: var(--alpha);
+  }
+  .column-wrapper {
+    display: flex;
+    justify-content: space-around;
+    transform: scale(2) rotate(45deg);
+  }
+  .column {
+    width: 30%;
+  }
+  video {
+    /* mix-blend-mode: difference; */
   }
 </style>
