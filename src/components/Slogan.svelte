49eee17b83a97dd72a75521c8a80e1c87775d9d2
yuichkun <yuichkun@gmail.com>
2023-11-17 02:15:46 +0900

    isolate slogan

---
+++ src/components/Slogan.svelte
@@ -0,0 +1,15 @@
+<span class="text-center md:text-5xl md:leading-loose text-white mincho">
+  <div>
+    <span class="font-bold"> 東京 </span>
+    から
+    <span class="font-bold"> 世界 </span>
+    へ
+  </div>
+  映像とクリエイティブテクノロジーの融合</span
+>
+
+<style>
+  .mincho {
+    font-family: "seiwa-do-l", serif;
+  }
+</style>
