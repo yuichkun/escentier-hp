e1b2ee82529cfecb30bd16bde952f225084dee00
yuichkun <yuichkun@gmail.com>
2023-11-21 03:07:34 +0900

    add mix blend mode to slogan

--- src/components/Slogan.svelte
+++ src/components/Slogan.svelte
@@ -1,7 +1,7 @@
 <div class="w-screen h-screen flex justify-center content-center">
   <object
     title="Escentier Slogan"
-    class="md:w-9/12 w-full h-full"
+    class="md:w-9/12 w-full h-full mix-blend-difference"
     type="image/svg+xml"
     data="/escentier-slogan.svg"
   />
